3de70a53318c41804efb1e1c5a0fa0fe12eb64d2
Mc L <gmclachlan45@gmail.com>
2023-07-04 10:07:33 -0700

    Update the black window

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,22 @@
 int main(void) {
     std::cout << "Hello Danmaku" << std::endl;
     GLFWwindow* window;
-
+	
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+	/* Add hints to specify OpenGL profile */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Danmaku Game", NULL, NULL);
-    if (!window)
-    {
+    window = glfwCreateWindow(800, 600, "Danmaku Game", NULL, NULL);
+    if (!window) {
+	    std::cout << "Failed to create window" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -23,7 +30,12 @@ int main(void) {
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)){
+	  std::cout << "Failed to initialize GLAD" << std::endl;
+	  return -1;
+	};
+
+	glViewport(0, 0, 800, 600);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)){
